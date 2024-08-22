8acd6f50dd682de2f320b2e47760c1dbca923be0
5u00n <surenhembram97@gmail.com>
2024-08-22 11:16:39 +0530

    chore: Update robot.txt, package.json, and index.html
    - Update robot.txt to include disallowing /cgi-bin/
    - Update package.json to add deploy script
    - Update index.html to replace Flowbite logo with SDA logo and update navigation links

--- dist/js/database.js
+++
@@ -1,8 +0,0 @@
-document.onload = function() {
-    //load json file and print to console
-    fetch('./../data/SiteData.json')
-    .then(response => response.json())
-    .then(data => console.log(data));
-
-
-}
--- js/global.js
+++ js/global.js
@@ -4,8 +4,16 @@ window.onload = function () {
         .then(response => response.json())
         .then(data => {
             document.getElementById('address').innerHTML = data.address;
-            document.getElementById('timings').innerHTML = data.timings;
             document.getElementById('phoneEmail').innerHTML = data.phone1 + "<br>" + data.phone2 + "<br>" + data.email;
+
+            let timingData = "";
+            Object.values(data.timings).map((timing) => {
+                timingData += timing + "<br>";
+                console.log(timing);
+            });
+
+            document.getElementById('timings').innerHTML = timingData;
+
         })
         .catch(error => console.error('Error fetching data:', error));
 };
