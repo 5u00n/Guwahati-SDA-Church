08d312339109aca128683eca723a2e1896d1fe80
5u00n <surenhembram97@gmail.com>
2024-08-27 21:08:39 +0530

    Update aboutChurch data in SiteData.json and display it in the footer

--- js/global.js
+++ js/global.js
@@ -1,5 +1,4 @@
 window.onload = function () {
-    console.log('Global JS loaded');
 
 
     // Determine if the current URL is a GitHub Pages URL
@@ -28,6 +27,30 @@ window.onload = function () {
             });
 
             document.getElementById('timings').innerHTML = timingData;
+            document.getElementById('footerAboutChurch').innerHTML = data.aboutChurch;
+
+            //contacts page data
+            let contactAddress=document.getElementById('contactAddress');
+            let contactPhone1=document.getElementById('contactPhone1');
+            let contactPhone2=document.getElementById('contactPhone2');
+            let contactEmail=document.getElementById('contactEmail');
+
+            if(contactAddress){
+                contactAddress.innerHTML=data.address;
+            }
+            if(contactPhone1){
+                contactPhone1.innerHTML=data.phone1;
+            }
+            if(contactPhone2){
+                contactPhone2.innerHTML=data.phone2;
+            }
+            if(contactEmail){
+                contactEmail.innerHTML=data.email;
+                contactEmail.href="mailto:"+data.email;
+            }
+            
+
+
 
         })
         .catch(error => console.error('Error fetching data:', error));
